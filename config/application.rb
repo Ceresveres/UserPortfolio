252d35f1662cb2f0afe35e69feceb86ba1a62d56
ceresveres <ceresveres@gmail.com>
2019-01-18 17:48:36 -0900

    Fixed devise initializor

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module UserPortfolio
     config.generators.system_tests = nil
 
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
